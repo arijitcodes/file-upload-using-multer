53b51a0df8af67283f4ce84a1785eb6a2ee5b004
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2024-03-02 23:04:36 +0530

    docs(upload): added Comments

--- src/routes/v1/upload.js
+++ src/routes/v1/upload.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 
+// Configure Multer Storage Settings
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/");
@@ -11,13 +12,15 @@ const storage = multer.diskStorage({
     cb(null, fileName);
   },
 });
+
+// Setup Multer Middleware with Storage Settings
 // const upload = multer({ dest: "public/" });
 const upload = multer({ storage: storage });
 
 // Controllers Imports
 const { uploadFile } = require("../../controllers/v1/upload");
 
-// Setup the Routes Endpoints and Controlelr
+// Setup the Route's Endpoints and Controlelr
 router.route("/").post(upload.single("file"), uploadFile);
 
 // Export the Router
